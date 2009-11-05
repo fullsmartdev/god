d1b61e8a4e0e171f866e23019ef900efa2aef820
HideAKi <hideaki.chain@gmail.com>
2009-11-05 01:47:24 -0800

    Make sure the driver is shutdown on Task#unregister!.

--- lib/god/diagnostics.rb
+++ lib/god/diagnostics.rb
@@ -12,25 +12,25 @@ end
 class BleakHouseDiagnostic
   LOG_FILE = File.join(File.dirname(__FILE__), *%w[.. .. logs bleak.log])
   
+  class << self
+    attr_accessor :logger
+  end
+  
   def self.install
-    require 'snapshot'
-    self.spin
+    require 'bleak_house'
+    self.logger = BleakHouse::Logger.new
+    File.delete(LOG_FILE) rescue nil
   end
   
-  def self.snapshot
-    @count ||= 0
-    filename = "/tmp/god-bleak-%s-%03i.dump" % [Process.pid,@count]
-    STDERR.puts "** BleakHouse: working..."
-    BleakHouse.ext_snapshot(filename, 3)
-    STDERR.puts "** BleakHouse: complete\n** Bleakhouse: Run 'bleak #{filename}' to analyze."
-    @count += 1
+  def self.snapshot(name)
+    self.logger.snapshot(LOG_FILE, name, false) if self.logger
   end
   
-  def self.spin(delay = 60)
+  def self.spin(delay = 1)
     Thread.new do
       loop do
-        sleep(delay)
         self.snapshot
+        sleep(delay)
       end
     end
   end
